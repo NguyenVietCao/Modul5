f0a6da0e914796c70d11c73895430f8588b3ed2d
cao <vietcao2510@gmail.com>
2023-07-19 15:16:31 +0700

    fixed

--- BT1/bai-tap-sinh-vien/src/index.js
+++ BT1/bai-tap-sinh-vien/src/index.js
@@ -7,31 +7,37 @@ import './index.css';
 
 const students = [
   {
+    id:1,
     company: 'Alfreds Futterkiste',
     contact: 'Maria Anders',
     country: 'Germany'
   },
   {
+    id:2,
     company: 'Centro comercial Moctezuma',
     contact: 'Francisco Chang',
     country: 'Mexico'
   },
   {
+    id:3,
     company: 'Ernst Handel',
     contact: 'Roland Mendel',
     country: 'Austria'
   },
   {
+    id:4,
     company: 'Island Trading',
     contact: 'Helen Bennett',
     country: 'UK'
   },
   {
+    id:5,
     company: 'Laughing Bacchus Winecellars',
     contact: 'Yoshi Tannamuri',
     country: 'Canada'
   },
   {
+    id:6,
     company: 'Magazzini Alimentari Riuniti',
     contact: 'Giovanni Rovelli',
     country: 'Italy'
@@ -50,7 +56,7 @@ root.render(
     </thead>
     <tbody>
     {students.map(student => (
-          <tr>
+          <tr key={student.id}>
             <td>{student.company}</td>
             <td>{student.contact}</td>
             <td>{student.country}</td>
